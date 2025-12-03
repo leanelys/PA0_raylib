7b51eb5d5b15cab71514c58b108119b17b104719
leanelys <leanelys.gonzalez@upr.edu>
2025-12-03 11:20:36 -0400

    Small refactor

--- src/Program.cpp
+++ src/Program.cpp
@@ -5,52 +5,61 @@ Program::Program(){
     bounce.Load("audio/bounce.mp3");
     // Setting volume
     bounce.SetVolume(0.5f);
-    // Load a font from from the resources directory
-    scary.Load("fonts/Scary.ttf");
-    // Text that will be moving
-    text = "PA0";
-    textSize = scary.MeasureText(text, 20.0f, 0.0f);
-    
-    // Load a texture from the resources directory
-	wabbit = LoadTexture("images/wabbit_alpha.png");
 
+    // Load a font from the resources directory
+    scary.Load("fonts/Scary.ttf");
+
+    // Title text at center of screen
+    titleText = "Welcome to Advanced Programming!";
+    titleTextSize = MeasureTextEx(scary, titleText.c_str(), 20, 0);
+
+    // Text that will be moving
+    bouncingText = "PA0";
+    bouncingTextSize = scary.MeasureText(bouncingText, 20.0f, 0.0f);
+    
+    // Speed and position of moving text
     xSpeed = 2;
     ySpeed = 2;
-    xPos = GetRandomValue(0, GetScreenWidth()-textSize.x);
-    yPos = GetRandomValue(0, GetScreenHeight()-textSize.y);
+    xPos = GetRandomValue(0, GetScreenWidth()-bouncingTextSize.x);
+    yPos = GetRandomValue(0, GetScreenHeight()-bouncingTextSize.y);
 
+    // Load a texture from the resources directory
+	wabbit = LoadTexture("images/wabbit_alpha.png");
 }
 
 void Program::Update(){
     // Avoid text from going offscreen
     // If text hits a wall, go the opposite direction
-    if (xPos + textSize.x >= GetScreenWidth() || xPos <= 0){
+    if (xPos + bouncingTextSize.x >= GetScreenWidth() || xPos <= 0){
         xSpeed *= -1;
         PlaySound(bounce);
     }
-    if (yPos + textSize.y >= GetScreenHeight() || yPos <= 0){
+    if (yPos + bouncingTextSize.y >= GetScreenHeight() || yPos <= 0){
         ySpeed *= -1;
         PlaySound(bounce);
     }
 
     xPos += xSpeed;
-    yPos += ySpeed;
-    
-    // For debugging
-    // std::cout << xPos << " x " << yPos << "y " << GetScreenHeight() << " yScr " << GetScreenWidth() << " xScr " << std::endl ;
+    yPos += ySpeed;    
 }
 
 void Program::Draw(){
     // Set background color
     ClearBackground(GRAY);
+
     // Draw title text in the middle of the screen
-    std::string topText = "Welcome to Advanced Programming!";
-    raylib::Vector2 topTextSize = MeasureTextEx(scary, topText.c_str(), 20, 0);
-    scary.DrawText(topText, (GetScreenWidth()/2 - topTextSize.GetX()/2), GetScreenHeight()/2.0f, 20.0f, 0.0f, WHITE);
+    raylib::Vector2 position = raylib::Vector2((GetScreenWidth()/2 - titleTextSize.GetX()/2), (GetScreenHeight()/2 - titleTextSize.GetY()/2));
+    scary.DrawText(titleText, position, 20.0f, 0.0f, WHITE);
+
     // Draw the moving text
-    scary.DrawText(text, xPos, yPos, 20.0f, 0.0f, WHITE);
+    scary.DrawText(bouncingText, xPos, yPos, 20.0f, 0.0f, WHITE);
+
     // Bunny :)
     wabbit.Draw(GetScreenWidth()/2 - wabbit.width/2, GetScreenHeight()/2 - wabbit.height/2 + 40, WHITE);
+
+    // Coordinate plane used to check if text and image are centered (uncomment to see)
+    // DrawLine(0, GetScreenHeight()/2, GetScreenWidth(), GetScreenHeight()/2, WHITE);
+    // DrawLine(GetScreenWidth()/2, 0, GetScreenWidth()/2, GetScreenHeight(), WHITE);
 }
 
 void Program::Unload(){
--- src/Program.hpp
+++ src/Program.hpp
@@ -6,16 +6,19 @@
 
 class Program {
   private:
-    std::string text;  // Text that will be bouncing around the screen
-    int xPos;          // Position of the text in the x axis
-    int yPos;          // Position of the text in the y axis
-    int xSpeed;        // Speed of the text in the x axis (horizontal)
-    int ySpeed;        // Speed of the text in the y axis (vertical)
+    std::string bouncingText;  // Text that will be bouncing around the screen
+    int xPos;                  // Position of the bouncing text in the x axis
+    int yPos;                  // Position of the bouncing text in the y axis
+    int xSpeed;                // Speed of the bouncing text in the x axis (horizontal)
+    int ySpeed;                // Speed of the bouncing text in the y axis (vertical)
 
-    raylib::Vector2 textSize;  // Size of bouncing text
-    raylib::Font scary;        // Font of text
-    raylib::Sound bounce;      // Text bounce sound effect
-    raylib::Texture wabbit;    // Image
+    std::string titleText;          // Text displayed in the center of the screen
+    raylib::Vector2 titleTextSize;  // Size of title text
+
+    raylib::Vector2 bouncingTextSize;  // Size of bouncing text
+    raylib::Font scary;                // Font of all text
+    raylib::Sound bounce;              // Text bounce sound effect
+    raylib::Texture wabbit;            // Image
     
   public:
     Program();   // Program constructor
