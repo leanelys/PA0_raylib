cc8dfa6107135b140decdd6234aee28c23c4ef60
leanelys <leanelys.gonzalez@upr.edu>
2025-12-03 11:23:26 -0400

    Fix typos

--- src/Program.hpp
+++ src/Program.hpp
@@ -9,20 +9,20 @@ class Program {
     std::string bouncingText;  // Text that will be bouncing around the screen
     int xPos;                  // Position of the bouncing text in the x axis
     int yPos;                  // Position of the bouncing text in the y axis
-    int xSpeed;                // Speed of the bouncing text in the x axis (horizontal)
-    int ySpeed;                // Speed of the bouncing text in the y axis (vertical)
+    int xSpeed;                // Speed of the bouncing text in the x axis (horizontal speed)
+    int ySpeed;                // Speed of the bouncing text in the y axis (vertical speed)
 
     std::string titleText;          // Text displayed in the center of the screen
     raylib::Vector2 titleTextSize;  // Size of title text
 
     raylib::Vector2 bouncingTextSize;  // Size of bouncing text
-    raylib::Font scary;                // Font of all text
+    raylib::Font scary;                // Font used for all text
     raylib::Sound bounce;              // Text bounce sound effect
     raylib::Texture wabbit;            // Image
     
   public:
     Program();   // Program constructor
-    ~Program();  // Program desctructor
+    ~Program();  // Program destructor
 
     void Update();
     void Draw();
